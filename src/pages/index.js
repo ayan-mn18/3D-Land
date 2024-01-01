f450a705a86f7b73fa9c0f170ec52aa61a47b642
ayan-mn18 <ayanmansuri2001@gmail.com>
2024-01-01 16:56:35 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './Home';
+import Projects from './Projects';
+import Contact from './Contact';
+import About from './About';
+
+export {
+    Home,
+    About,
+    Projects,
+    Contact,
+}
